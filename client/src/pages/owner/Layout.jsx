3a06e19ca5c7228b8b69659bb5c8a39abaa67df8
TranThiThanhPhuong <phuongsuga333@gmail.com>
2026-05-20 13:40:12 +0700

    đồng bộ admin và owner

--- client/src/pages/owner/Layout.jsx
+++ client/src/pages/owner/Layout.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import Sidebar from '../../components/owner/Sidebar'
 import Navbar from '../../components/owner/Navbar'
 import Footer from '../../components/owner/Footer'
 
 const Layout = () => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true)
     return (
         <div className="flex h-screen bg-[#F5F5F5] overflow-hidden">
-            <Sidebar />
+            <Sidebar isOpen={isSidebarOpen} setIsOpen={setIsSidebarOpen} />
             <div className="flex-1 flex flex-col overflow-hidden relative">
                 {/* Lớp nền Texture Noise */}
                 <div className="absolute inset-0 opacity-[0.03] pointer-events-none z-50 bg-[url('https://www.transparenttextures.com/patterns/natural-paper.png')]"></div>
